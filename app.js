77ecb3c6759c795431573a186cb78ccf8e76c744
Alex Lee <alexclee7@gmail.com>
2026-04-28 20:31:23 -0700

    Added javascript file - app.js

---
+++ app.js
@@ -0,0 +1,5 @@
+const contentText = document.querySelector('.content-text');
+
+document.addEventListener('DOMContentLoaded', function() {
+    contentText.innerHtml = "Thisi is the content.";
+}, false);
