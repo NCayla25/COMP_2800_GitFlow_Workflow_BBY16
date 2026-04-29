7f8e49b114c1c14dcdd2978543c4e426071c420a
Alex Lee <alexclee7@gmail.com>
2026-04-28 20:53:14 -0700

    Revert "Added javascript file - app.js"
    This reverts commit 77ecb3c6759c795431573a186cb78ccf8e76c744.

--- app.js
+++
@@ -1,5 +0,0 @@
-const contentText = document.querySelector('.content-text');
-
-document.addEventListener('DOMContentLoaded', function() {
-    contentText.innerHtml = "Thisi is the content.";
-}, false);
